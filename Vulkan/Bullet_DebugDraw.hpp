e6d046e7f882649a5ba1d6f0170786fa82ac51ed
Kyle J Klouzal <kyle.klouzal@gmail.com>
2019-11-27 05:35:43 -0700

    Bullet DebugDraw Framework

---
+++ Vulkan/Bullet_DebugDraw.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+class VulkanBTDebugDraw : public btIDebugDraw
+{
+	int m_debugMode;
+
+public:
+
+	VulkanBTDebugDraw() {}
+	virtual ~VulkanBTDebugDraw() {}
+
+	virtual void drawLine(const btVector3& from, const btVector3& to, const btVector3& fromColor, const btVector3& toColor) {}
+
+	virtual void drawLine(const btVector3& from, const btVector3& to, const btVector3& color) {}
+
+	virtual void drawSphere(const btVector3& p, btScalar radius, const btVector3& color) {}
+
+	virtual void drawTriangle(const btVector3& a, const btVector3& b, const btVector3& c, const btVector3& color, btScalar alpha) {}
+
+	virtual void drawContactPoint(const btVector3& PointOnB, const btVector3& normalOnB, btScalar distance, int lifeTime, const btVector3& color) {}
+
+	virtual void reportErrorWarning(const char* warningString) { printf("[Bullet]: %s\n", warningString); }
+	virtual void draw3dText(const btVector3& location, const char* textString) {}
+	virtual void setDebugMode(int debugMode) { m_debugMode = debugMode; }
+	virtual int getDebugMode() const { return m_debugMode; }
+
+};
--- Vulkan/SceneGraph.hpp
+++ Vulkan/SceneGraph.hpp
@@ -2,6 +2,7 @@
 
 #include "Import_FBX.hpp"
 #include "btBulletDynamicsCommon.h"
+#include "Bullet_DebugDraw.hpp"
 
 //
 //	Forward Declare Individual Scene Nodes
@@ -54,6 +55,12 @@ class SceneGraph {
 	btSequentialImpulseConstraintSolver* solver;
 
 	btDiscreteDynamicsWorld* dynamicsWorld;
+
+	//make sure to re-use collision shapes among rigid bodies whenever possible!
+	btAlignedObjectArray<btCollisionShape*> collisionShapes;
+#ifdef _DEBUG
+	VulkanBTDebugDraw BTDebugDraw;
+#endif
 
 public:
 	VulkanDriver* _Driver = VK_NULL_HANDLE;
@@ -150,6 +157,10 @@ void SceneGraph::validate(const uint32_t& currentImage) {
 			SceneNodes[i]->drawFrame(primaryCommandBuffers[currentImage]);
 		}
 
+#ifdef _DEBUG
+		dynamicsWorld->debugDrawWorld();
+#endif
+
 		_Driver->DrawExternal(currentImage);
 
 		vkCmdEndRenderPass(primaryCommandBuffers[currentImage]);
@@ -223,9 +234,94 @@ SceneGraph::SceneGraph(VulkanDriver* Driver) : _Driver(Driver), _ImportFBX(new I
 	dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, overlappingPairCache, solver, collisionConfiguration);
 
 	dynamicsWorld->setGravity(btVector3(0, -10, 0));
+#ifdef _DEBUG
+	dynamicsWorld->setDebugDrawer(&BTDebugDraw);
+#endif
+
+	///create a few basic rigid bodies
+
+//the ground is a cube of side 100 at position y = -56.
+//the sphere will hit it at y = -6, with center at -5
+	{
+		btCollisionShape* groundShape = new btBoxShape(btVector3(btScalar(50.), btScalar(50.), btScalar(50.)));
+
+		collisionShapes.push_back(groundShape);
+
+		btTransform groundTransform;
+		groundTransform.setIdentity();
+		groundTransform.setOrigin(btVector3(0, -56, 0));
+
+		btScalar mass(0.);
+
+		//rigidbody is dynamic if and only if mass is non zero, otherwise static
+		bool isDynamic = (mass != 0.f);
+
+		btVector3 localInertia(0, 0, 0);
+		if (isDynamic)
+			groundShape->calculateLocalInertia(mass, localInertia);
+
+		//using motionstate is optional, it provides interpolation capabilities, and only synchronizes 'active' objects
+		btDefaultMotionState* myMotionState = new btDefaultMotionState(groundTransform);
+		btRigidBody::btRigidBodyConstructionInfo rbInfo(mass, myMotionState, groundShape, localInertia);
+		btRigidBody* body = new btRigidBody(rbInfo);
+
+		//add the body to the dynamics world
+		dynamicsWorld->addRigidBody(body);
+	}
+
+	{
+		//create a dynamic rigidbody
+
+		//btCollisionShape* colShape = new btBoxShape(btVector3(1,1,1));
+		btCollisionShape* colShape = new btSphereShape(btScalar(1.));
+		collisionShapes.push_back(colShape);
+
+		/// Create Dynamic Objects
+		btTransform startTransform;
+		startTransform.setIdentity();
+
+		btScalar mass(1.f);
+
+		//rigidbody is dynamic if and only if mass is non zero, otherwise static
+		bool isDynamic = (mass != 0.f);
+
+		btVector3 localInertia(0, 0, 0);
+		if (isDynamic)
+			colShape->calculateLocalInertia(mass, localInertia);
+
+		startTransform.setOrigin(btVector3(2, 10, 0));
+
+		//using motionstate is recommended, it provides interpolation capabilities, and only synchronizes 'active' objects
+		btDefaultMotionState* myMotionState = new btDefaultMotionState(startTransform);
+		btRigidBody::btRigidBodyConstructionInfo rbInfo(mass, myMotionState, colShape, localInertia);
+		btRigidBody* body = new btRigidBody(rbInfo);
+
+		dynamicsWorld->addRigidBody(body);
+	}
 }
 
 SceneGraph::~SceneGraph() {
+	//remove the rigidbodies from the dynamics world and delete them
+	for (int i = dynamicsWorld->getNumCollisionObjects() - 1; i >= 0; i--)
+	{
+		btCollisionObject* obj = dynamicsWorld->getCollisionObjectArray()[i];
+		btRigidBody* body = btRigidBody::upcast(obj);
+		if (body && body->getMotionState())
+		{
+			delete body->getMotionState();
+		}
+		dynamicsWorld->removeCollisionObject(obj);
+		delete obj;
+	}
+
+	//delete collision shapes
+	for (int j = 0; j < collisionShapes.size(); j++)
+	{
+		btCollisionShape* shape = collisionShapes[j];
+		collisionShapes[j] = 0;
+		delete shape;
+	}
+
 	//delete dynamics world
 	delete dynamicsWorld;
 	//delete solver
